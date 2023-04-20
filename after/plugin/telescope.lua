029ed4016cf316803bbfa7b7cdf4d0c5521d37db
alejandro waumann <awaumann@gmail.com>
2023-04-20 01:14:29 -0500

    Add telescope git status

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,12 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pf', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_string({ search = vim.fn.input('Grep > ') });
-end)
 
 vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {})
 
 vim.keymap.set('n', '<leader>po', builtin.vim_options, {})
 vim.keymap.set('n', '<leader>pk', builtin.keymaps, {})
+
+vim.keymap.set('n', '<leader>pg', builtin.git_status, {})

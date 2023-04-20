464c4dca26e65d4807ab0a81d84aef4c91c04608
alejandro waumann <awaumann@gmail.com>
2023-04-20 00:11:59 -0500

    Add zenmode

--- lua/godyl/packer.lua
+++ lua/godyl/packer.lua
@@ -19,6 +19,8 @@ return require('packer').startup(function(use)
   use { 'Mofiqul/dracula.nvim' }
   use { 'tpope/vim-fugitive' }
 
+  use { 'folke/zen-mode.nvim' }
+
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
